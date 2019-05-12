94f108592f6afebf9d0d60696baee470bf7d67e6
TheMasterOfMagic <18811569079@163.com>
2019-05-12 22:57:44 +0800

    完成了DNS服务的安装与测试脚本

--- chap0x06/scripts/DNS/test.sh
+++ chap0x06/scripts/DNS/test.sh
@@ -9,4 +9,4 @@ cd "$(dirname "$0")" || (exit 0)
 source ../functions.sh
 [[ "$(whoami)" == root ]] || exit_because "root previledge is required"
 
-warn "not implemented"
+[[ "$(dig +short @${server} example.sec.cuc.edu.cn 2> /dev/null)" == "192.168.0.175" ]] || exit_because "dns answer is not correct"
