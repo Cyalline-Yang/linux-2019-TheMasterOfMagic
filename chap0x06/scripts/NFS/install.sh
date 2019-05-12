42fc2cca74bea9bb416d2a3c677fa6668546b9cf
TheMasterOfMagic <18811569079@163.com>
2019-05-12 14:41:55 +0800

    完成了NFS服务的安装与测试脚本

--- chap0x06/scripts/NFS/install.sh
+++ chap0x06/scripts/NFS/install.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "$0")" || (exit 0)
 source ../functions.sh
 [[ "$(whoami)" == root ]] || exit_because "root previledge is required"
 
-warn "not implemented"
+# make sure nfs-kernel-server is installed
+apt-get install -y nfs-kernel-server &> /dev/null || exit_because "failed to install nfs-kernel-server"
+
+# make sure sharing directories and files exist
+mkdir -p /var/nfs/readonly_dir
+echo > /var/nfs/readonly_dir/readonly_file
+mkdir -p /var/nfs/writable_dir
+echo > /var/nfs/writable_dir/writable_file
+
+# make sure sharing dierctories and files are exported
+tee /etc/exports > /dev/null << EOF
+/var/nfs/readonly_dir *(ro,sync,no_subtree_check,insecure)
+/var/nfs/writable_dir *(rw,sync,no_subtree_check,insecure)
+EOF
+
+# restart the service to apply
+systemctl restart nfs-server
--- chap0x06/scripts/NFS/test.sh
+++ chap0x06/scripts/NFS/test.sh
@@ -9,4 +9,32 @@ cd "$(dirname "$0")" || (exit 0)
 source ../functions.sh
 [[ "$(whoami)" == root ]] || exit_because "root previledge is required"
 
-warn "not implemented"
+# make sure nfs-common is installed
+apt-get install -y nfs-common &> /dev/null || exit_because "failed to install nfs-common"
+
+# make sure sharing directories exist and are mounted
+## umount and rm if necesssary
+##################################
+## 32: no mount point specified ##
+## 64: not mounted              ##
+##################################
+umount /var/nfs/* &> /dev/null || [[ $? -eq 32 || $? -eq 64 ]] || exit_because "failed to umount existing mount point"
+rm -rf /var/nfs/*
+## mkdir and mount
+mkdir -p /var/nfs/readonly_dir
+mkdir -p /var/nfs/writable_dir && chmod -R 777 /var/nfs/writable_dir
+mount -o nfsvers=3 "${server}":/var/nfs/readonly_dir /var/nfs/readonly_dir &> /dev/null || exit_because "failed to mount the readonly directory"
+mount -o nfsvers=3 "${server}":/var/nfs/writable_dir /var/nfs/writable_dir &> /dev/null || exit_because "failed to mount the writable directory"
+
+# test
+## readonly part
+cd /var/nfs/readonly_dir &> /dev/null || exit_because "failed to cd the readonly_dir"
+[[ "$(ls readonly_file 2> /dev/null)" == readonly_file ]] || exit_because "failed to do ls in the readonly_dir"
+cat readonly_file &> /dev/null || exit_because "failed to read content of the readonly_file"
+touch new_file 2> /dev/null && exit_because "failed to prevent writing to the readonly_dir"
+## writable part
+cd /var/nfs/writable_dir &> /dev/null || exit_because "failed to cd the writable_dir"
+[[ "$(ls writable_file 2> /dev/null)" == writable_file ]] || exit_because "failed to do ls in the writable_dir"
+cat writable_file &> /dev/null || exit_because "failed to read content of the writable_file"
+touch new_file 2> /dev/null || exit_because "failed to create file in the writable_dir"
+echo >> writable_file || exit_because "failed to write into the writable_file"
