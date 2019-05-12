b886a19af6704a44e0d2bdafbe4077a162843d61
TheMasterOfMagic <18811569079@163.com>
2019-05-12 19:08:15 +0800

    fixed bugs

--- chap0x06/scripts/NFS/test.sh
+++ chap0x06/scripts/NFS/test.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-declare server
-[[ "${server}" != "" ]] || exit_because "\${server} needs to be specified"
-
 cd "$(dirname "$0")" || (exit 0)
 source ../functions.sh
+
+declare server
+[[ "${server}" != "" ]] || exit_because "\${server} needs to be specified"
 [[ "$(whoami)" == root ]] || exit_because "root previledge is required"
 
 warn "not implemented"
