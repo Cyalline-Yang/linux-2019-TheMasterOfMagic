1c73d5fd93a43da9b7ff593a9c5164276c18cf5a
TheMasterOfMagic <18811569079@163.com>
2019-05-12 22:15:38 +0800

    完成了DHCP服务的安装与测试脚本

--- chap0x06/scripts/DHCP/install.sh
+++ chap0x06/scripts/DHCP/install.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
 
+set -e 
+
 cd "$(dirname "$0")" || (exit 0)
 source ../functions.sh
 [[ "$(whoami)" == root ]] || exit_because "root previledge is required"
 
-warn "not implemented"
+# make sure isc-dhcp-server is installed
+apt-get install -y isc-dhcp-server &> /dev/null || exit_because "failed to install isc-dhcp-server"
+
+# set ip addr
+cp 01-netcfg.yaml /etc/netplan/01-netcfg.yaml
+cat >> /etc/netplan/01-netcfg.yaml << EOF
+    enp0s8:
+      addresses: [10.20.50.1/24]
+      gateway4: 10.20.50.1
+      dhcp4: no
+EOF
+netplan apply || exit_because "failed to apply netplan"
+
+# modify dhcp config files
+echo INTERFACESv4=\"enp0s8\" > /etc/default/isc-dhcp-server
+cp ./dhcpd.conf /etc/dhcp/dhcpd.conf
+cat >> /etc/dhcp/dhcpd.conf << EOF
+subnet 10.20.50.0 netmask 255.255.255.0 {
+    range 10.20.50.2 10.20.50.254;
+    option subnet-mask 255.255.255.0;
+    option routers 10.20.50.1;
+    option broadcast-address 10.20.50.255;
+    default-lease-time 600;
+    max-lease-time 7200;
+}
+EOF
+
+# restart the service to apply the lastest config
+systemctl restart isc-dhcp-server
